fc8376bbb9f0ab5975ea76e95f4a681c577365f6
luis laurente <llaurente4@gmail.com>
2017-02-25 01:30:18 -0500

    Initial commit

---
+++ src/com/languaguexsapp/models/BaseEntity.java
@@ -0,0 +1,7 @@
+package com.languaguexsapp.models;
+
+/**
+ * Created by laurente on 25/02/2017.
+ */
+public class BaseEntity {
+}
---
+++ src/com/languaguexsapp/services/LenguaguexsService.java
@@ -0,0 +1,7 @@
+package com.languaguexsapp.services;
+
+/**
+ * Created by laurente on 25/02/2017.
+ */
+public class LenguaguexsService {
+}
