2f1fd5f8eb52d218ef4679bca9d1c1dfdbb60992
fcosmeo <frankcosmesystem@gmail.com>
2017-03-05 22:15:27 -0500

    Base de Datos: Se anula el Id auto incremental de las tablas y se crea la tabla tables_id para el manejo del id de las tablas mediante el procedimiento almacenado sp_getIdTable

--- Bd_MySQL_LanguageXS_Insert.sql
+++ Bd_MySQL_LanguageXS_Insert.sql
@@ -1,237 +1,154 @@
-Create Database bd_languagexs;
+DROP DATABASE IF EXISTS bd_languagexs;
 
-use bd_languagexs;
+CREATE DATABASE bd_languagexs;
 
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: localhost    Database: bd_languagexs
--- ------------------------------------------------------
--- Server version	5.7.14-log
+USE bd_languagexs;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `languages`
---
+DROP TABLE IF EXISTS languages;
 
-DROP TABLE IF EXISTS `languages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `languages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(150) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE languages (
+  id int(11) NOT NULL,
+  description varchar(150) NOT NULL,
+  PRIMARY KEY (id)
+) ;
 
---
--- Dumping data for table `languages`
---
+INSERT INTO languages VALUES (1,'English'),(2,'French'),(3,'Chinese'),(4,'Japanese'),(5,'Spanish');
 
-LOCK TABLES `languages` WRITE;
-/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
-INSERT INTO `languages` VALUES (1,'English'),(2,'French'),(3,'Chinese'),(4,'Japanese'),(5,'Spanish');
-/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `lesson_students`
---
+DROP TABLE IF EXISTS levels;
 
-DROP TABLE IF EXISTS `lesson_students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lesson_students` (
-  `lesson_id` int(11) NOT NULL,
-  `person_id` int(11) NOT NULL,
-  `registration_date` date DEFAULT NULL,
-  `score_student` int(11) DEFAULT NULL,
-  `score_teacher` int(11) DEFAULT NULL,
-  PRIMARY KEY (`lesson_id`,`person_id`),
-  KEY `people_lesson_students_fk` (`person_id`),
-  CONSTRAINT `lessons_lesson_students_fk` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `people_lesson_students_fk` FOREIGN KEY (`person_id`) REFERENCES `people` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE levels (
+  id int(11) NOT NULL,
+  description varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+) ;
 
---
--- Dumping data for table `lesson_students`
---
+INSERT INTO levels VALUES (1,'Basic'),(2,'Intermediate'),(3,'Advanced');
 
-LOCK TABLES `lesson_students` WRITE;
-/*!40000 ALTER TABLE `lesson_students` DISABLE KEYS */;
-INSERT INTO `lesson_students` VALUES (1,3,'2017-03-02',NULL,NULL),(2,4,'2017-03-02',NULL,NULL),(3,3,'2017-03-09',NULL,NULL);
-/*!40000 ALTER TABLE `lesson_students` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `lessons`
---
+DROP TABLE IF EXISTS status;
 
-DROP TABLE IF EXISTS `lessons`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lessons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `skill_id` int(11) NOT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  `status_id` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `status_lessons_fk` (`status_id`),
-  KEY `skills_lessons_fk` (`skill_id`),
-  CONSTRAINT `skills_lessons_fk` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `status_lessons_fk` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE status (
+  id int(11) NOT NULL,
+  description varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+) ;
 
---
--- Dumping data for table `lessons`
---
+INSERT INTO status VALUES (0,'Inactivo'),(1,'Activo');
 
-LOCK TABLES `lessons` WRITE;
-/*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
-INSERT INTO `lessons` VALUES (1,1,'2017-03-06','2017-03-14',1),(2,1,'2017-03-15','2017-03-31',1),(3,2,'2017-03-15','2017-03-31',1),(4,2,'2017-04-10','2017-05-18',1);
-/*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `levels`
---
+DROP TABLE IF EXISTS people;
 
-DROP TABLE IF EXISTS `levels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `levels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE people (
+  id int(11) NOT NULL,
+  first_name varchar(150) NOT NULL,
+  last_name varchar(150) NOT NULL,
+  email varchar(150) DEFAULT NULL,
+  user varchar(20) NOT NULL,
+  password varchar(100) NOT NULL,
+  registration_date date DEFAULT NULL,
+  status_id int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (id),
+  KEY status_people_fk (status_id),
+  CONSTRAINT status_people_fk FOREIGN KEY (status_id) REFERENCES status (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ;
 
---
--- Dumping data for table `levels`
---
+INSERT INTO people VALUES (1,'Frank','Cosme Oropeza','fcosmeo@utp.edu.pe','fcosmeo','123456','2017-03-04',1),(2,'Luis','Laurente Areas','llaurentea@utp.edu.pe','llaurentea','123456','2017-03-03',1),(3,'Leonardo','Caycho','lcaycho@utp.edu.pe','lcaycho','123456','2017-03-06',1),(4,'Juan','Quezada','jquezada@utp.edu.pe','jquezada','123456','2017-03-04',1);
 
-LOCK TABLES `levels` WRITE;
-/*!40000 ALTER TABLE `levels` DISABLE KEYS */;
-INSERT INTO `levels` VALUES (1,'Basic'),(2,'Intermediate'),(3,'Advanced');
-/*!40000 ALTER TABLE `levels` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `people`
---
+DROP TABLE IF EXISTS skills;
 
-DROP TABLE IF EXISTS `people`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `people` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(150) NOT NULL,
-  `last_name` varchar(150) NOT NULL,
-  `email` varchar(150) DEFAULT NULL,
-  `user` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `registration_date` date DEFAULT NULL,
-  `status_id` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `status_people_fk` (`status_id`),
-  CONSTRAINT `status_people_fk` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE skills (
+  id int(11) NOT NULL,
+  person_id int(11) NOT NULL,
+  language_id int(11) NOT NULL,
+  level_id int(11) NOT NULL,
+  price decimal(12,2) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY languages_skills_fk (language_id),
+  KEY levels_skills_fk (level_id),
+  KEY people_skills_fk (person_id),
+  CONSTRAINT languages_skills_fk FOREIGN KEY (language_id) REFERENCES languages (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT levels_skills_fk FOREIGN KEY (level_id) REFERENCES levels (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT people_skills_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ;
 
---
--- Dumping data for table `people`
---
+INSERT INTO skills VALUES (1,1,1,3,15.25),(2,3,4,2,30.21);
 
-LOCK TABLES `people` WRITE;
-/*!40000 ALTER TABLE `people` DISABLE KEYS */;
-INSERT INTO `people` VALUES (1,'Frank','Cosme Oropeza','fcosmeo@utp.edu.pe','fcosmeo','123456','2017-03-04',1),(2,'Luis','Laurente Areas','llaurentea@utp.edu.pe','llaurentea','123456','2017-03-03',1),(3,'Leonardo','Caycho','lcaycho@utp.edu.pe','lcaycho','123456','2017-03-06',1),(4,'Juan','Quezada','jquezada@utp.edu.pe','jquezada','123456','2017-03-04',1);
-/*!40000 ALTER TABLE `people` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `skills`
---
+DROP TABLE IF EXISTS lessons;
 
-DROP TABLE IF EXISTS `skills`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `skills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `person_id` int(11) NOT NULL,
-  `language_id` int(11) NOT NULL,
-  `level_id` int(11) NOT NULL,
-  `price` decimal(12,2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `languages_skills_fk` (`language_id`),
-  KEY `levels_skills_fk` (`level_id`),
-  KEY `people_skills_fk` (`person_id`),
-  CONSTRAINT `languages_skills_fk` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `levels_skills_fk` FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `people_skills_fk` FOREIGN KEY (`person_id`) REFERENCES `people` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE lessons (
+  id int(11) NOT NULL,
+  skill_id int(11) NOT NULL,
+  start_date date DEFAULT NULL,
+  end_date date DEFAULT NULL,
+  status_id int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (id),
+  KEY status_lessons_fk (status_id),
+  KEY skills_lessons_fk (skill_id),
+  CONSTRAINT skills_lessons_fk FOREIGN KEY (skill_id) REFERENCES skills (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT status_lessons_fk FOREIGN KEY (status_id) REFERENCES status (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ;
 
---
--- Dumping data for table `skills`
---
+INSERT INTO lessons VALUES (1,1,'2017-03-06','2017-03-14',1),(2,1,'2017-03-15','2017-03-31',1),(3,2,'2017-03-15','2017-03-31',1),(4,2,'2017-04-10','2017-05-18',1);
 
-LOCK TABLES `skills` WRITE;
-/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
-INSERT INTO `skills` VALUES (1,1,1,3,15.25),(2,3,4,2,30.21);
-/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `status`
---
+DROP TABLE IF EXISTS lesson_students;
 
-DROP TABLE IF EXISTS `status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `status` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE lesson_students (
+  lesson_id int(11) NOT NULL,
+  person_id int(11) NOT NULL,
+  registration_date date DEFAULT NULL,
+  score_student int(11) DEFAULT NULL,
+  score_teacher int(11) DEFAULT NULL,
+  PRIMARY KEY (lesson_id,person_id),
+  KEY lesson_lesson_students_fk (lesson_id),
+  KEY people_lesson_students_fk (person_id),
+  CONSTRAINT lessons_lesson_students_fk FOREIGN KEY (lesson_id) REFERENCES lessons (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT people_lesson_students_fk FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ;
 
---
--- Dumping data for table `status`
---
+INSERT INTO lesson_students VALUES (1,3,'2017-03-02',NULL,NULL),(2,4,'2017-03-02',NULL,NULL),(3,3,'2017-03-09',NULL,NULL);
 
-LOCK TABLES `status` WRITE;
-/*!40000 ALTER TABLE `status` DISABLE KEYS */;
-INSERT INTO `status` VALUES (1,'Activo'),(0,'Inactivo');
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Dumping events for database 'bd_languagexs'
---
+DROP TABLE IF EXISTS tables_id;
 
---
--- Dumping routines for database 'bd_languagexs'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE tables_id(
+	name varchar(150) NOT NULL,
+    id int(11) NOT NULL DEFAULT '0',
+    PRIMARY KEY (name)
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO tables_id VALUES('languages',5),('lessons',4),('levels',3),('people',4),('skills',2),('status',1);
 
--- Dump completed on 2017-03-04  1:51:24
+
+DROP PROCEDURE IF EXISTS sp_getIdTable;
+DELIMITER $$
+CREATE PROCEDURE sp_getIdTable(
+  IN table_name varchar(150),
+  OUT table_id int
+)
+BEGIN
+	DECLARE rowcount int default 0;
+    
+	SELECT count(*),max(id)
+    INTO rowcount,table_id
+    FROM tables_id
+    WHERE name=table_name;   
+	
+    if rowcount=0 or isnull(rowcount) then    
+		set table_id=1;
+        
+		INSERT INTO tables_id VALUES(table_name,table_id);
+	
+	else
+		set table_id=table_id+1;
+        
+		UPDATE tables_id
+			SET id=table_id
+		WHERE name=table_name;
+	end if;
+END$$
+DELIMITER ;
